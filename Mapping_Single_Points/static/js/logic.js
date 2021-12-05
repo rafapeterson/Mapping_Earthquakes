c76db5cc2a75aa553ee0b8cb1974258944923dfa
rafapeterson <rafaaapeterson@gmail.com>
2021-12-05 11:32:36 -0700

    updated logic.js

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -3,6 +3,15 @@ console.log("working");
 // Create the map object with a center & zoom level/
 let map = L.map('mapid').setView([40.7, -94.5], 4);
 
+// Add a marker to the map for LA
+// Add circle
+L.circleMarker([34.0522, -118.2437], {
+    color: 'black',
+    fillColor: 'yellow',
+    radius: 300
+ }).addTo(map);
+
+
 //tile layer that'll be the background of the map
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
